02da1661be3414f41a13590d775a89517301519b
Juan Mariscal <j.m.mariscal03@gmail.com>
2021-10-15 14:59:44 -0700

    Changed from textField to SearchBar

--- NBAstats/ViewController.swift
+++ NBAstats/ViewController.swift
@@ -7,26 +7,28 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
-    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var questionLabel: UILabel!
     
+    var dataFiltered: [String] = []
     
     
     @IBAction func searchButtonPressed(_ sender: Any) {
         
         switch segmentedControl.selectedSegmentIndex {
         case 0:
-            let yearToSearch = Int(searchTextField.text!)!
+            let yearToSearch = Int(searchBar.text!)!
             answerLabel.text = ModelController.shared.checkChampion(year: yearToSearch)
         case 1:
-            let team = searchTextField.text!
+            let team = searchBar.text!
             answerLabel.text = String(ModelController.shared.firstYearWon(team: team))
         case 2:
-            let team = searchTextField.text!
+            let team = searchBar.text!
             let yearText = ModelController.shared.checkYearsForChampion(team: team)
             answerLabel.text = yearText.map({String($0)}).joined(separator: ", ")
         default:
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '9.0'
+
+target 'NBAstats' do
+  use_frameworks!
+
+  pod 'DropDown', '2.3.2'
+end
