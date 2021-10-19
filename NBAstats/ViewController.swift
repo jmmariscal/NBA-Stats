c8966b7eaddec2e7ffb1547f76a7cc777b9e2ded
Juan Mariscal <j.m.mariscal03@gmail.com>
2021-10-19 14:20:14 -0700

    Refactored switch cases and questions label. With proper keyboard for each question.

--- NBAstats/ViewController.swift
+++ NBAstats/ViewController.swift
@@ -10,25 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var searchTextField: UITextField!
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var questionLabel: UILabel!
     
     var dataFiltered: [String] = []
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateSearchFor(questionType: .champByYear)
+    }
+    
+    private var inputQuestionType = QuestionType.firstYear {
+        didSet {
+            updateSearchFor(questionType: inputQuestionType)
+        }
+        
+    }
+    
     
     @IBAction func searchButtonPressed(_ sender: Any) {
         
         switch segmentedControl.selectedSegmentIndex {
         case 0:
-            let yearToSearch = Int(searchBar.text!)!
+            let yearToSearch = Int((searchTextField.text!.trimmingCharacters(in: .whitespaces)))!
             answerLabel.text = ModelController.shared.checkChampion(year: yearToSearch)
         case 1:
-            let team = searchBar.text!
+            let team = searchTextField.text!.trimmingCharacters(in: .whitespaces)
             answerLabel.text = String(ModelController.shared.firstYearWon(team: team))
         case 2:
-            let team = searchBar.text!
+            let team = searchTextField.text!.trimmingCharacters(in: .whitespaces)
             let yearText = ModelController.shared.checkYearsForChampion(team: team)
             answerLabel.text = yearText.map({String($0)}).joined(separator: ", ")
         default:
@@ -39,16 +51,42 @@ class ViewController: UIViewController {
     @IBAction func segmentedIndexTapped(_ sender: Any) {
         switch segmentedControl.selectedSegmentIndex {
         case 0:
-            questionLabel.text = "Check for Champion by year: "
+            inputQuestionType = .champByYear
         case 1:
-            questionLabel.text = "Check the first year a team was a Champion: "
+            inputQuestionType = .firstYear
         case 2:
-            questionLabel.text = "Check all years a team won a Championship: "
+            inputQuestionType = .allYears
         default:
             break
         }
     }
     
+    func keyboardTypeAndLabelText() {
+        
+    }
     
+    
+    enum QuestionType {
+        case firstYear, champByYear, allYears
+    }
+    
+    func updateSearchFor(questionType: QuestionType) {
+        func updateKeyboarType(to keyboardType: UIKeyboardType, andQuestionText questionText: String) {
+            searchTextField.text = ""
+            searchTextField.resignFirstResponder()
+            searchTextField.keyboardType = keyboardType
+            searchTextField.becomeFirstResponder()
+            questionLabel.text = questionText
+        }
+        
+        switch questionType {
+        case .firstYear:
+            updateKeyboarType(to: .default, andQuestionText: "Check the first year a team won a championship")
+        case .champByYear:
+            updateKeyboarType(to: .numberPad, andQuestionText: "Check for Champion by year: ")
+        case .allYears:
+            updateKeyboarType(to: .default, andQuestionText: "Find all the years a team won a championship")
+        }
+    }
 }
 
